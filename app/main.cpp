106d8964f91a370bb3a1c28b1938d72ef3a822f3
alexen <enakaev.aleksey@yandex.ru>
2023-10-06 23:57:30 +0300

    app/main: Реализована первая часть многопоточного сервера NMH
    Что сделано:
    - реализован слушатель входящих запросов который:
      * читает входные данные из STDIN по протоколу NMH;
      * парсит JSON и формирует структуру Request;
      * помещает запрос в очередь входящих запросов;
    - реализован обработчик запросов, который:
      * слушает очередь входящих запросов;
      * забирает запрос из очереди и, в качестве "обработки", просто
        распечатывает его в лог.
    
    Все пока что производится в одном файле main.cpp, но после реализации
    всего функционала все будет разнесено по своим местам.

--- app/main.cpp
+++ app/main.cpp
@@ -7,7 +7,10 @@
 #include <sstream>
 #include <iostream>
 
+#include <boost/bind.hpp>
+#include <boost/thread.hpp>
 #include <boost/date_time.hpp>
+#include <boost/make_shared.hpp>
 #include <boost/throw_exception.hpp>
 #include <boost/exception/errinfo_errno.hpp>
 #include <boost/exception/enable_error_info.hpp>
@@ -20,37 +23,135 @@
 #include <nmh/nmh.h>
 #include <tools/logger/init.h>
 #include <tools/converter/hexlify.h>
+#include <tools/mt/queue.h>
 
 
-std::tuple< int, std::string > parseMessage( std::istream& is )
+using RequestPtr = boost::shared_ptr< struct Request >;
+
+struct Request {
+     static RequestPtr fromJson( std::istream& is )
+     {
+          boost::property_tree::ptree root;
+          boost::property_tree::read_json( is, root );
+
+          return boost::make_shared< Request >(
+               root.get< int >( "messageId" ),
+               root.get< std::string >( "content" ),
+               root.get< std::string >( "timestamp" )
+               );
+     }
+
+     Request( std::uint32_t msgId, std::string&& content, std::string&& tstamp )
+          : messageId{ msgId }
+          , content{ content }
+          , timestamp{ tstamp }
+     {}
+
+     std::uint32_t messageId;
+     std::string content;
+     std::string timestamp;
+};
+
+
+std::ostream& operator<<( std::ostream& os, const Request& rq )
 {
-     boost::property_tree::ptree root;
-     boost::property_tree::read_json( is, root );
-
-     return {
-          root.get< int >( "messageId" ),
-          root.get< std::string >( "content" )
-     };
+     return os
+          << "message: id: " << rq.messageId
+          << ", content: " << rq.content
+          << ", at " << rq.timestamp;
 }
 
 
-std::string makeResponseTo( const int messageId, boost::string_view message )
+using ResponsePtr = boost::shared_ptr< struct Response >;
+
+struct Response {
+     std::string toJson( const int messageId, boost::string_view message )
+     {
+          /// Делаем статический поток, но отдельный для каждого потока,
+          /// для чего используем boost::thread_specific_ptr<>
+          static boost::thread_specific_ptr< std::ostringstream > oss;
+          if( !oss.get() )
+          {
+               oss.reset( new std::ostringstream );
+          }
+
+          /// Поскольку буфер статический, очистку выполняем при каждом обращении.
+          /// Такой метод работы самый быстрый.
+          oss->str( {} );
+
+          boost::property_tree::ptree root;
+
+          root.put( "processId", getpid() );
+          root.put( "replyTo", messageId );
+          root.put( "status", "accepted" );
+          root.put( "sourceMessage", message );
+          root.put( "timestamp", boost::posix_time::to_iso_extended_string( boost::posix_time::second_clock::local_time() ) );
+
+          boost::property_tree::write_json( *oss, root );
+
+          return oss->str();
+     }
+
+     std::uint32_t messageId;
+     std::uint32_t inReplyTo;
+     std::string status;
+     std::string content;
+     std::string timestamp;
+};
+
+
+using RequestQueue = alexen::tools::mt::BlockingQueue< RequestPtr >;
+using ResponseQueue = alexen::tools::mt::BlockingQueue< ResponsePtr >;
+
+
+void requestListener( RequestQueue& requestQueue )
 {
-     static std::ostringstream ostr;
+     try
+     {
+          BOOST_LOG_TRIVIAL( info ) << "Start request listener";
 
-     ostr.str( {} );
+          std::stringstream request;
+          while( alexen::nmh::protocol::read( std::cin, request ) )
+          {
+               BOOST_LOG_TRIVIAL( debug )
+                    << "Received: " << request.str();
 
-     boost::property_tree::ptree root;
+               try
+               {
+                    requestQueue.push( Request::fromJson( request ) );
+               }
+               catch( const std::exception& )
+               {
+                    BOOST_LOG_TRIVIAL( error )
+                         << "exception: " << boost::current_exception_diagnostic_information();
+               }
 
-     root.put( "processId", getpid() );
-     root.put( "replyTo", messageId );
-     root.put( "status", "accepted" );
-     root.put( "sourceMessage", message );
-     root.put( "timestamp", boost::posix_time::to_iso_extended_string( boost::posix_time::second_clock::local_time() ) );
+               request.str( {} );
+          }
+     }
+     catch( const std::exception& )
+     {
+          BOOST_LOG_TRIVIAL( error )
+               << "exception: " << boost::current_exception_diagnostic_information();
+     }
+}
 
-     boost::property_tree::write_json( ostr, root );
 
-     return ostr.str();
+void requestProcessor( RequestQueue& requestQueue, ResponseQueue& )
+{
+     try
+     {
+          while( true )
+          {
+               const auto request = requestQueue.pop();
+               BOOST_LOG_TRIVIAL( info ) << "Processing: " << *request;
+          }
+     }
+     catch( const std::exception& )
+     {
+          BOOST_LOG_TRIVIAL( error )
+                    << "exception: " << boost::current_exception_diagnostic_information();
+     }
 }
 
 
@@ -61,38 +162,33 @@ int main()
           alexen::tools::logger::initFileLog( "/tmp/nmh_backend.log", boost::log::trivial::trace );
           alexen::tools::logger::initOstreamLog( std::cerr, boost::log::trivial::info );
 
-          std::stringstream request;
-          while( alexen::nmh::protocol::read( std::cin, request ) )
-          {
-               BOOST_LOG_TRIVIAL( info )
-                    << "Raw data incoming: " << request.str();
+          RequestQueue requestQueue;
+          ResponseQueue responseQueue;
 
-               try
-               {
-                    const auto& [ messageId, content ] = parseMessage( request );
-
-                    BOOST_LOG_TRIVIAL( info )
-                         << "Parsed message: id: " << messageId
-                         << ", content: " << std::quoted( content );
-
-                    alexen::nmh::protocol::write( makeResponseTo( messageId, content ), std::cout );
-               }
-               catch( const boost::property_tree::ptree_error& )
-               {
-                    static const std::string errorResponse =
-                         R"json({
-                              "state": "error",
-                              "reason": "Bad incoming message format"
-                         })json";
-
-                    BOOST_LOG_TRIVIAL( error )
-                         << "exception: " << boost::current_exception_diagnostic_information();
-                    alexen::nmh::protocol::write( errorResponse, std::cout );
-               }
-
-               request.str( {} );
-          }
-          BOOST_LOG_TRIVIAL( info ) << "Seems to finished working!";
+          boost::thread_group tg;
+          tg.create_thread( boost::bind( requestListener, boost::ref( requestQueue ) ) );
+          tg.create_thread(
+               boost::bind(
+                    requestProcessor,
+                    boost::ref( requestQueue ),
+                    boost::ref( responseQueue )
+                    )
+               );
+          tg.create_thread(
+               boost::bind(
+                    requestProcessor,
+                    boost::ref( requestQueue ),
+                    boost::ref( responseQueue )
+                    )
+               );
+          tg.create_thread(
+               boost::bind(
+                    requestProcessor,
+                    boost::ref( requestQueue ),
+                    boost::ref( responseQueue )
+                    )
+               );
+          tg.join_all();
      }
      catch( ... )
      {
